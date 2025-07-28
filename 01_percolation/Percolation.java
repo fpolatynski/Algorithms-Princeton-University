18323207fb12f255c445cb0f301d30694cfe5773
Polatynski Filip <filip.polatynski@aptiv.com>
2025-07-28 11:19:06 +0200

    Open site function

--- 01_percolation/Percolation.java
+++ 01_percolation/Percolation.java
@@ -18,7 +18,8 @@ public class Percolation {
         // Creates Union Find data structure for all cells in grid
         uf = new WeightedQuickUnionUF(n*n + 2);
         // Creates grid with all open cells
-        grid = new int[n][n];
+        grid = new boolean
+        [n][n];
         for (int row = 0; row < n; row++){
             for (int col = 0; col < n; col++)
                 grid[row][col] = false;
@@ -28,12 +29,27 @@ public class Percolation {
             // connect first row to begin
             uf.union(begin, i);
             // connect last row to end
-            uf.union(end, n-i-1)
+            uf.union(end, n-i-1);
         }
     }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col)
+    public void open(int row, int col){
+        // Check if its not open
+        if (!grid[row][col]){
+            // Open site
+            grid[row][col] = true;
+            // Create connections for all open neighbors
+            if (row > 0 && grid[row - 1][col]) 
+                uf.union(row_col2uf_idx(row, col), row_col2uf_idx(row - 1, col)); 
+            if (row < dimention - 1 && grid[row + 1][col]) 
+                uf.union(row_col2uf_idx(row, col), row_col2uf_idx(row + 1, col)); 
+            if (col > 0 && grid[row][col-1]) 
+                uf.union(row_col2uf_idx(row, col), row_col2uf_idx(row, col - 1)); 
+            if (col < dimention - 1 && grid[row][col-1]) 
+                uf.union(row_col2uf_idx(row, col), row_col2uf_idx(row, col + 1)); 
+        }
+    }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col){
@@ -44,14 +60,21 @@ public class Percolation {
     }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col)
+    public boolean isFull(int row, int col){return true;}
 
     // returns the number of open sites
-    public int numberOfOpenSites()
+    public int numberOfOpenSites(){
+        return count;
+    }
 
     // does the system percolate?
-    public boolean percolates()
+    public boolean percolates(){return true;}
+
+    // Convert row and col to Union Find index
+    private int row_col2uf_idx(int row, int col){
+        return dimention * row + col;
+    }
 
     // test client (optional)
-    public static void main(String[] args)
+    public static void main(String[] args){}
 }
