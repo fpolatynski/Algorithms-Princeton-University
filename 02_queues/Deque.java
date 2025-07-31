3d8ccbd625b02b67c7e5190338494162acf920c5
Polatynski Filip <filip.polatynski@aptiv.com>
2025-07-31 16:57:54 +0200

    adding, removing and structure of deque

---
+++ 02_queues/Deque.java
@@ -0,0 +1,87 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+    private Item[] array;
+    private int left = 0;
+    private int right = 0;
+
+    // construct an empty deque
+    public Deque() {
+        array = (Item[]) new Object[4];
+    }
+
+    // is the deque empty?
+    public boolean isEmpty(){return true;}
+
+    // return the number of items on the deque
+    public int size(){
+        return right - left;
+    }
+
+    // add the item to the front
+    public void addFirst(Item item) {
+        array[left % array.length] = item;
+        left--;
+    }
+
+    // add the item to the back
+    public void addLast(Item item) {
+        right++;
+        array[right % array.length] = item;
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst() {
+        left++;
+        return array[left % array.length];
+    }
+
+    // remove and return the item from the back
+    public Item removeLast() {
+        Item last = array[right % array.length];
+        right--;
+        return last;
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator(){return null;}
+
+    // unit testing (required)
+    public static void main(String[] args){
+        System.out.println("UNIT TESTS");
+
+        System.out.println("Constructor:");
+        Deque<String> sut = new Deque<String>();
+        System.out.println(sut.left == 0);
+        System.out.println(sut.right == 0);
+        System.out.println();
+
+        System.out.println("Size:");
+        System.out.println(sut.size() == 0);
+        sut.addFirst("Hi");
+        sut.addLast("Hi1");
+        System.out.println(sut.size() == 2);
+        System.out.println();
+
+        System.out.println("addFirst, removeFirst, addLast, removeLast");
+        System.out.println("Hi".equals(sut.removeFirst()));
+        System.out.println("Hi1".equals(sut.removeFirst()));
+        sut.addFirst("H2");
+        sut.addLast("Hi3");
+        sut.addFirst("Hi1");
+        sut.addLast("Hi4");
+        System.out.println("Hi4".equals(sut.removeLast()));
+        System.out.println("Hi3".equals(sut.removeLast()));
+        System.out.println();
+
+        System.out.println("Iterator");
+        Iterator<String> i = sut.iterator();
+        String ans = "";
+        while (i.hasNext()) {
+            String s = i.next();
+            ans += s;
+        }
+        System.out.println("Hi1Hi2".equals(ans));
+    }
+
+}
